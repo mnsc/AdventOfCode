efac8ddfe51251cdddc1c60214139ad2e6ef79a0
Mattias Blom <mattias.blom@omegapoint.se>
2020-12-01 12:14:51 +0100

    Day 1

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -10,17 +11,29 @@ namespace AdventOfCode.Solutions.Year2020
 
         public Day01() : base(01, 2020, "")
         {
-
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var input = Input.SplitByNewline().Select(i => int.Parse(i)).ToList();
+
+            for (int i = 0; i < input.Count - 1; i++)
+                for (int j = i + 1; j < input.Count; j++)
+                    if (input[i] + input[j] == 2020)
+                        return (input[i] * input[j]).ToString();
+            return "";
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var input = Input.SplitByNewline().Select(i => int.Parse(i)).ToList();
+
+            for (int i = 0; i < input.Count - 2; i++)
+                for (int j = i + 1; j < input.Count - 1; j++)
+                    for (int k = i + 1; k < input.Count; k++)
+                        if (input[i] + input[j] + input[k] == 2020)
+                            return (input[i] * input[j] * input[k]).ToString();
+            return "";
         }
     }
 }
