70bb79a75eecc527e022c8c85c57f1fd370a987f
Mattias Blom <mattias.blom@omegapoint.se>
2020-12-06 08:46:34 +0100

    Day 6 Part 1

--- AdventOfCode/Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,15 +8,44 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day06 : ASolution
     {
+        private string[][] _declarations;
 
         public Day06() : base(06, 2020, "")
         {
+//            DebugInput = @"abc
 
+//a
+//b
+//c
+
+//ab
+//ac
+
+//a
+//a
+//a
+//a
+
+//b";
+
+            _declarations = Input
+                .Split(new[] { "\r\r", "\n\n", "\r\n\r\n" }, StringSplitOptions.None)
+                .Select(group => group.Split(new[] { "\r", "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToArray())
+                .ToArray();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return _declarations
+                .Select(
+                    group =>
+                    group.SelectMany(person => person.ToCharArray())
+                    .Distinct()
+                    .Count()
+                )
+                .Sum()
+                .ToString();
+
         }
 
         protected override string SolvePartTwo()
