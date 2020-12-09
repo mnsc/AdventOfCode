8e1a2763988af5e77a6a6f45068c4bf54c2c3206
Mattias Blom <mattias.blom@omegapoint.se>
2020-12-09 07:11:14 +0100

    Day 9 first part

--- AdventOfCode/Solutions/Year2020/Day09/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day09/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,14 +8,65 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day09 : ASolution
     {
+        int preamble = 25;
 
         public Day09() : base(09, 2020, "")
         {
+//            DebugInput = @"
+//35
+//20
+//15
+//25
+//47
+//40
+//62
+//55
+//65
+//95
+//102
+//117
+//150
+//182
+//127
+//219
+//299
+//277
+//309
+//576
+//";
+            if (DebugInput != null)
+            {
+                preamble = 5;
+            }
 
         }
 
         protected override string SolvePartOne()
         {
+            var input = Input.SplitByNewline().Select(long.Parse).ToArray();
+
+            for (int chk = preamble; chk < input.Length; chk++)
+            {
+                bool match = false;
+                var toCheck = input[chk];
+                for (int i = chk - 1; i > chk - preamble; i--)
+                {
+                    for (int j = i - 1; j > chk - preamble - 1; j--)
+                    {
+                        if (input[i] + input[j] == toCheck)
+                        {
+                            match = true;
+                            goto Hell;
+                        }
+                    }
+                }
+            Hell:
+                if (!match)
+                    return toCheck.ToString();
+
+            }
+
+
             return null;
         }
 
