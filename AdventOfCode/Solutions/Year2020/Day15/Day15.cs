f77cd1cf486b9bf8050474b25e59e88339a16c12
Mattias Blom <mattias.blom@omegapoint.se>
2020-12-18 21:10:10 +0100

    Day 15 part 2. Speedy.

--- AdventOfCode/Solutions/Year2020/Day15/Day15.cs
+++ AdventOfCode/Solutions/Year2020/Day15/Day15.cs
@@ -33,16 +33,34 @@ namespace AdventOfCode.Solutions.Year2020
         {
             const int startSize = 6;
             const int gameSize = 30000000;
-            int[] game = new int[startSize] { 10, 16, 6, 0, 1, 17 };
-            Array.Resize(ref game, gameSize);
+            var mostRecentIndex = new Dictionary<int, int>();
 
+            int[] game = new int[startSize] { 10, 16, 6, 0, 1, 17 };
+            for (int i = 0; i < game.Length -1; i++)
+            {
+                mostRecentIndex[game[i]] = i;
+            }
+            Array.Resize(ref game, gameSize);
+            
             for (int i = startSize; i < game.Length; i++)
             {
-                int lastspoken = game[i - 1];
-                var mostrecentIndex = Array.FindLastIndex(game, i - 2, i => i == lastspoken);
-                game[i] = mostrecentIndex == -1 ? 0 : i - (mostrecentIndex + 1);
+                var lastSpokenIndex = i - 1;
+                int lastSpokenNumber = game[lastSpokenIndex];
+
+                // Hmm, I think I heard this before....
+                if(mostRecentIndex.ContainsKey(lastSpokenNumber))
+                {
+                    game[i] = lastSpokenIndex - mostRecentIndex[lastSpokenNumber];
+                    mostRecentIndex[lastSpokenNumber] = lastSpokenIndex;
+                }
+                // Never heard of it!
+                else
+                {
+                    game[i] = 0;
+                    mostRecentIndex[lastSpokenNumber] = lastSpokenIndex;
+                }
             }
-            return game[gameSize - 1].ToString();
+            return $"I needed {mostRecentIndex.Keys.Count} lookbacks to see the answer: {game[gameSize - 1]}";
         }
     }
 }
