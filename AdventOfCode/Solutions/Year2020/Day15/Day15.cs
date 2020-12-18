1153cd35afb06fc532bc02742251b2c170123595
Mattias Blom <mattias.blom@omegapoint.se>
2020-12-18 21:09:42 +0100

    Day 15 part 1 bruteforce No worky for part 2

--- AdventOfCode/Solutions/Year2020/Day15/Day15.cs
+++ AdventOfCode/Solutions/Year2020/Day15/Day15.cs
@@ -10,17 +10,39 @@ namespace AdventOfCode.Solutions.Year2020
 
         public Day15() : base(15, 2020, "")
         {
-
+         
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            const int startSize = 6;
+            const int gameSize = 2020;
+            int[] game = new int[startSize] { 10, 16, 6, 0, 1, 17 };
+            Array.Resize(ref game, gameSize);
+
+            for (int i = startSize; i < game.Length; i++)
+            {
+                int lastspoken = game[i - 1];
+                var mostrecentIndex = Array.FindLastIndex(game, i - 2, i => i == lastspoken);
+                game[i] = mostrecentIndex == -1 ? 0 : i - (mostrecentIndex + 1) ;
+            }
+            return game[gameSize-1].ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            const int startSize = 6;
+            const int gameSize = 30000000;
+            int[] game = new int[startSize] { 10, 16, 6, 0, 1, 17 };
+            Array.Resize(ref game, gameSize);
+
+            for (int i = startSize; i < game.Length; i++)
+            {
+                int lastspoken = game[i - 1];
+                var mostrecentIndex = Array.FindLastIndex(game, i - 2, i => i == lastspoken);
+                game[i] = mostrecentIndex == -1 ? 0 : i - (mostrecentIndex + 1);
+            }
+            return game[gameSize - 1].ToString();
         }
     }
 }
